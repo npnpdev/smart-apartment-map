b46ac1e601b1be9141ba2e3be757d3741ddf2e04
a-hlebowicz <aleksanderhlebowicz@gmail.com>
2026-03-01 19:13:41 +0100

    Konfiguracja API: DRF/CORS + testowy endpoint /api/health
    dodano routing /api w config/urls.py, dodano api/urls.py oraz endpoint health, włączono rest_framework i corsheaders w settings, dopuszczono host 'backend'

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "0.0.0.0",
+    "backend",
 ]
 
 # Application definition
@@ -41,10 +42,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django.contrib.gis",
+    # 3rd party
+    "rest_framework",
+    "corsheaders",
+    # local apps
+    "api",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +131,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#  API / CORS (DEV) 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+# ogranicz CORS tylko do /api
+#CORS_URLS_REGEX = r"^/api/.*$"
+
+# DRF 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
